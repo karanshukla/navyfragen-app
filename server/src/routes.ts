63d53d523bb6ef3c0cba6c77c4512e967aeacafc
Karz <karan_shukla@live.com>
2025-05-27 15:16:29 +0000

    update to support railway caddy proxy

--- server/src/routes.ts
+++ server/src/routes.ts
@@ -36,10 +36,10 @@ function checkValidation(
 export const createRouter = (ctx: AppContext) => {
   const router = express.Router();
 
-  // Mount all sub-routers at /api
-  router.use("/api", authRoutes(ctx, handler, checkValidation));
-  router.use("/api", messageRoutes(ctx, handler, checkValidation));
-  router.use("/api", profileRoutes(ctx, handler, checkValidation));
+  // in the frontend the API URL must include the /api prefix
+  router.use(authRoutes(ctx, handler, checkValidation));
+  router.use(messageRoutes(ctx, handler, checkValidation));
+  router.use(profileRoutes(ctx, handler, checkValidation));
 
   return router;
 };
