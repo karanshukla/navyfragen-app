8e1fecf06d115f5dd3eb057104322d32fb873b5f
Karz <karan_shukla@live.com>
2025-06-08 09:10:45 -0400

    boostrap for tests

---
+++ server/src/tests/test-bootstrap.ts
@@ -0,0 +1,12 @@
+// test-bootstrap.ts
+// Sets dummy environment variables for CI and local test runs
+process.env.CLIENT_URL ||= "http://localhost";
+process.env.COOKIE_SECRET ||= "testsecret";
+process.env.DB_PATH ||= ":memory:";
+process.env.EXPORT_HTML_URL ||= "http://localhost:3033/";
+process.env.HOST ||= "localhost";
+process.env.NODE_ENV ||= "test";
+process.env.OAUTH_TOKEN_SECRET ||= "0123456789abcdef0123456789abcdef";
+process.env.PDS_HOST ||= "http://localhost:2583";
+process.env.PORT ||= "3000";
+process.env.POSTGRESQL_URL ||= "postgres://user:pass@localhost:5432/db";
